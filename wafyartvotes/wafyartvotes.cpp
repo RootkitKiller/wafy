6ff3ed9bc2357078692a43cf9ea9c169d5c6f1d5
mooninwater <wingsnorth@hotmail.com>
2018-07-31 20:55:15 +0800

    add vote

--- wafyartvotes/wafyartvotes.cpp
+++ wafyartvotes/wafyartvotes.cpp
@@ -1,36 +1,55 @@
 #include "wafyartvotes.hpp"
 
+// 生成该合约内唯一的id
 uint128_t wafyartvotes::getid(uint64_t id){
     uint128_t scopeid = N(wafyartvotes);
-    print(scopeid);
     uint128_t result  = (scopeid<<64) +id;
-    print("ssssssssss");
-    print(result);
     return result;
 }
+// 抵押代币，只能用wafyarttoken账户调用
 void wafyartvotes::staketit(account_name byname,account_name accname,uint64_t amount){
     // 检查签名是否匹配
     require_auth(byname);
     eosio_assert(byname==N(wafyarttoken),"错误：此帐号没有权限调用此action");
 
-    alltickets ticmul(_self,_self);
-    auto ticit=ticmul.find(accname);
+    // 更新账户票统计信息
+    accounticks accticmul(_self,_self);
+    auto accticit=accticmul.find(accname);
     
-    if(ticit!=ticmul.end()){
-        ticmul.modify(ticit,_self,[&](auto &obj){
-            obj.total=obj.total+amount;
-            obj.balance=obj.balance+amount;
+    if(accticit!=accticmul.end()){
+        accticmul.modify(accticit,_self,[&](auto &obj){
+            obj.totaltick=obj.totaltick+amount;
+            obj.unvotetick=obj.unvotetick+amount;
         });
     }else{
-        ticmul.emplace(_self,[&](auto& obj){
+        accticmul.emplace(_self,[&](auto& obj){
             obj.byname=accname;
-            obj.total=amount;
-            obj.balance=amount;
-            obj.locking=0;
-            obj.unstake=0;
+            obj.totaltick=amount;
+            obj.unvotetick=amount;
+            obj.votetick=0;
+            obj.unstaketick=0;
+        });
+    }
+    // 更新项目票统计信息
+    allticks allticmul(_self,_self);
+    auto allticit=allticmul.find(_self);
+    if(allticit!=allticmul.end()){
+        allticmul.modify(allticit,_self,[&](auto &obj){
+            obj.unvotetick=obj.unvotetick+amount;
+        });
+    }else{
+        allticmul.emplace(_self,[&](auto& obj){
+            obj.byname=_self;
+            obj.paytick=0;
+            obj.votetick=0;
+            obj.unvotetick=amount;
+            obj.unstaketick=0;
+            obj.addtick=0;
+            obj.buyramtick=0;
         });
     }
 }
+// 设置解锁代币的状态，解锁完成时调用
 void wafyartvotes::setstats(account_name byname,account_name accname,uint64_t id){
     require_auth(byname);
 
@@ -38,20 +57,36 @@ void wafyartvotes::setstats(account_name byname,account_name accname,uint64_t id
 
     unstakes unstamul(_self,accname);
     auto unstait=unstamul.find(id);
-
+    // 更新解锁MZP列表
     eosio_assert(unstait!=unstamul.end(),"错误：未发现该条抵押记录");
     unstamul.modify(unstait,_self,[&](auto &obj){
         obj.isend=true;
     });
+    // 更新用户票统计信息
+    accounticks accticmul(_self,_self);
+    auto accticit=accticmul.find(accname);
+    eosio_assert(accticit!=accticmul.end(),"错误：该用户没有抵押过票");
+    accticmul.modify(ticit,_self,[&](auto &obj){
+        obj.unstaketick=obj.unstaketick-amount;
+    });
+    // 更新项目票统计信息
+    allticks allticmul(_self,_self);
+    auto allticit=allticmul.find(_self);
+    eosio_assert(allticit!=allticmul.end(),"错误：未发现项目票统计信息")
+    allticmul.modify(allticit,_self,[&](auto &obj){
+        obj.unvotetick  = obj.unvotetick  - amount;
+        obj.unstaketick = obj.unstaketick + amount;
+    });
 }
+// 解锁代币，由用户调用
 void wafyartvotes::unstaketit(account_name byname,uint64_t amount){
     require_auth(byname);
 
-    alltickets ticmul(_self,_self);
-    auto ticit=ticmul.find(byname);
+    accounticks accticmul(_self,_self);
+    auto accticit=accticmul.find(byname);
 
-    eosio_assert(ticit!=ticmul.end(),"错误：该用户没有抵押过票");
-    eosio_assert(ticit->balance>amount,"错误：该用户没有足够的票解锁");
+    eosio_assert(accticit!=accticmul.end(),"错误：该用户没有抵押过票");
+    eosio_assert(accticit->unvotetick>amount,"错误：该用户没有足够的票解锁");
     eosio_assert(amount>10000,"错误：防止恶意ddos攻击，最少解锁1 MZ的投票 ");
 
     //生成合约唯一id
@@ -60,12 +95,11 @@ void wafyartvotes::unstaketit(account_name byname,uint64_t amount){
     senmul.emplace(_self,[&](auto &obj){
         obj.id=id;
     });
-
     uint128_t unstakeid=getid(id);
 
+    // 更新解锁MZP列表
     unstakes unstamul(_self,byname);
     uint64_t unstaid=unstamul.available_primary_key();
-
     unstamul.emplace(_self,[&](auto &obj){
         obj.id=unstaid;
         obj.timestamp=now();
@@ -73,13 +107,22 @@ void wafyartvotes::unstaketit(account_name byname,uint64_t amount){
         obj.isend=false;
         obj.unstakeid=unstakeid;
     });
-
-    ticmul.modify(ticit,_self,[&](auto &obj){
-        obj.total=obj.total-amount;
-        obj.balance=obj.balance-amount;
-        obj.unstake=obj.unstake+amount;
+    // 更新用户票统计信息
+    accticmul.modify(ticit,_self,[&](auto &obj){
+        obj.totaltick=obj.totaltick-amount;
+        obj.unvotetick=obj.unvotetick-amount;
+        obj.unstaketick=obj.unstaketick+amount;
+    });
+    // 更新项目票统计信息
+    allticks allticmul(_self,_self);
+    auto allticit=allticmul.find(_self);
+    eosio_assert(allticit!=allticmul.end(),"错误：未发现项目票统计信息")
+    allticmul.modify(allticit,_self,[&](auto &obj){
+        obj.unvotetick  = obj.unvotetick  - amount;
+        obj.unstaketick = obj.unstaketick + amount;
     });
 
+    // 设置延时交易
     transaction ttrans;
     symbol_type MZSYMBOL = symbol_type(string_to_symbol(4, "MZ"));
 
@@ -98,26 +141,42 @@ void wafyartvotes::unstaketit(account_name byname,uint64_t amount){
     ttrans.send(unstakeid,_self);
 
 }
+// 撤销解锁代币调用
 void wafyartvotes::delunstake(account_name byname,uint64_t id){
     require_auth(byname);
 
     unstakes unstamul(_self,byname);
     auto unstait=unstamul.find(id);
-
     eosio_assert(unstait!=unstamul.end(),"错误：不存在该解锁记录");
 
-    alltickets ticmul(_self,_self);
-    auto ticit=ticmul.find(byname);
-    eosio_assert(ticit!=ticmul.end(),"错误：该用户没有抵押记录");
-
-    ticmul.modify(ticit,_self,[&](auto& obj){
-        obj.total=obj.total+unstait->amount;
-        obj.balance=obj.balance+unstait->amount;
+    // 更新用户票信息
+    accounticks accticmul(_self,_self);
+    auto accticit=accticmul.find(byname);
+    eosio_assert(accticit!=accticmul.end(),"错误：该用户没有抵押记录");
+    accticmul.modify(accticit,_self,[&](auto& obj){
+        obj.totaltick   = obj.totaltick   + unstait->amount;
+        obj.unvotetick  = obj.unvotetick  + unstait->amount;
+        obj.unstaketick = obj.unstaketick - unstait->amount;
     });
     uint128_t unstakeid=unstait->unstakeid;
+    // 更新解锁MZP列表
     unstamul.erase(unstait);
+    // 更新项目票信息
+    allticks allticmul(_self,_self);
+    auto allticit=allticmul.find(_self);
+    eosio_assert(allticit!=allticmul.end(),"错误：未发现项目票统计信息")
+    allticmul.modify(allticit,_self,[&](auto &obj){
+        obj.unvotetick  = obj.unvotetick  + amount;
+        obj.unstaketick = obj.unstaketick - amount;
+    });
 
     //取消延迟交易
     cancel_deferred(unstakeid);
 }
-EOSIO_ABI( wafyartvotes, (staketit)(unstaketit)(setstats)(delunstake))
+// 给文章投票
+void voteart(account_name byname,uint64_t votenum,account_name catename,uint64_t artid){
+    require_auth(byname);
+
+    
+}
+EOSIO_ABI( wafyartvotes, (staketit)(unstaketit)(setstats)(delunstake)(voteart))
--- wafyartvotes/wafyartvotes.hpp
+++ wafyartvotes/wafyartvotes.hpp
@@ -1,6 +1,23 @@
 /**
  *  @file
  *  @copyright defined in eos/LICENSE.txt
+ *  票由MZ token抵押得到，以下简称MZP MZ Power
+ *  MZP 有三种状态
+ *  状态1：投票态，指该MZP参与了投票
+ *  状态2：闲置态，指该MZP置换过来后未参与投票，未解锁。闲置态是默认的状态。
+ *  状态3：解锁态，参与了解锁的MZP，解锁成功，MZP减少，Token增加。解锁可以撤销。
+ * 
+ *  关于MZP的操作，实质上是MZP在以上三种状态下的转换
+ *  抵押MZP：  Token ----> MZP 闲置态               基于账户操作
+ *  解锁MZP：  MZP 闲置态 ----> Token               基于账户操作
+ *  投票MZP：  MZP 闲置态 ----> MZP 投票态           基于账户操作
+ *  撤销投票：  MZP 投票态 ----> MZP 闲置态           基于账户操作
+ *  
+ *  奖励MZP用来给参与投票的用户、审核员、价值评论者作为激励。奖励MZP = 支付MZP + 增发MZP  奖励MZP 分为四部分
+ *  价值评论者：支付MZP*40%+增发MZP                  基于文章操作
+ *  投票参与者：支付MZP*40%                          基于分类操作  当该分类投票奖励超过界限值 VOTEREWORD 分发奖励 
+ *  审核员：    支付MZP*15%                         基于分类操作  同投票参与者奖励一起分发
+ *  资源支付金：支付MZP*5%                           基于项目操作  用于奖励RAM、CPU、NET、磁盘提供者
  */
 #pragma once
 
@@ -11,8 +28,9 @@
 
 using namespace eosio;
 using namespace std;
-const uint64_t VOTELIMIT = 10000;
+const uint64_t VOTELIMIT = 10000;       //投票界限，最少投一票1MZP 1.0000 MZP 10000 MMZP
 const uint64_t AUDITLIMIT = 1;
+const uint64_t VOTEREWORD = 10000;      //分发投票奖励界限，最少达到1票，再分发 1.0000 MZP
 const uint64_t UNSTAKETIME = 30;
 
 class wafyartvotes : public eosio::contract {
@@ -20,56 +38,77 @@ class wafyartvotes : public eosio::contract {
         using contract::contract;
 
         // scope为 _self 每个用户的票数统计
-        // @abi table alltickets i64
-        struct allticket{
+        // @abi table accounticks i64
+        struct accountick{
             account_name byname;
-            uint64_t total;
-            uint64_t balance;
-            uint64_t locking;
-            uint64_t unstake;
+            uint64_t totaltick;
+            uint64_t unvotetick;                    //闲置态
+            uint64_t votetick;                      //投票态
+            uint64_t unstaketick;                   //解锁态
 
             uint64_t primary_key()const { return byname;}
-            uint64_t get_total()const { return total;}
-            uint64_t get_bala()const { return balance;}
-            uint64_t get_lock()const { return locking;}
+            uint64_t get_total()const { return totaltick;}
 
-            EOSLIB_SERIALIZE(allticket,(byname)(total)(balance)(locking)(unstake))
+            EOSLIB_SERIALIZE(accountick,(byname)(totaltick)(unvotetick)(votetick)(unstaketick))
         };
-        typedef multi_index<N(alltickets),allticket,
-            indexed_by<N(bytotal),const_mem_fun<allticket,uint64_t,&allticket::get_total>>,
-            indexed_by<N(bybala),const_mem_fun<allticket,uint64_t,&allticket::get_bala>>,
-            indexed_by<N(bylock),const_mem_fun<allticket,uint64_t,&allticket::get_lock>>
-            > alltickets;
-        
-        // scope 为 _self 
-        // @abi table catepots i64
-        struct catepot{
+        typedef multi_index<N(accounticks),accountick,
+            indexed_by<N(bytotal),const_mem_fun<accountick,uint64_t,&accountick::get_total>>,
+            > accounticks;
+
+        // scope为account_name  每个用户在不同分类下的投票分布
+        // @abi table accvoteinfos i64
+        struct accvoteinfo{
             account_name catename;
-            uint64_t totalvote;
-            uint64_t totalrewo;
-            uint64_t bestrewo;
-            uint64_t voterewo;
-            uint64_t auditrewo;
-            uint64_t buyramrewo;
+            uint64_t  votetick;
 
             uint64_t primary_key()const { return catename;}
-            uint64_t get_vote()const { return totalvote;}
-            uint64_t get_rewo()const { return totalrewo;}
+        }
+        typedef multi_index<N(accvoteinfos),accvoteinfo> accvoteinfos;
+
+        // scope为account_name 每个用户的投票列表
+        // @abi table votelists i64
+        struct votelist{
+            uint64_t id;
+            account_name catename;
+            uint64_t amount;                //参投的MZP数量
+            uint64_t artid;                 //文章id
+            uint64_t timestamp;             //时间戳，用来撤销投票
+
+            uint64_t primary_key()const { return id;}
+        }
+        typedef multi_index<N(votelists),votelist> votelists;
+
+        // 文章
+
+        // 审核者
+
+        // 提案
+
+        // scope 为 _self 
+        // @abi table cateticks i64
+        struct catetick{
+            account_name catename;
+            uint64_t paytick;               //该分类赚取的收益，来自于发表文章的人，消耗的票，按比例分发出去
+            uint64_t votetick;              //该分类获得的投票数，部分奖励根据投票数分发给投票人
+            uint64_t addtick;               //根据投票，增发的票据，用来奖励价值评论者。
+
+            uint64_t voterewo;              //投票者该分得的奖励
+            uint64_t auditrewo;             //审核者该分得的奖励  此两种奖励是独立于文章之外的
+
+            uint64_t primary_key()const { return catename;}
+            uint64_t get_vote()const { return votetick;}
+            uint64_t get_pay()const { return paytick;}
 
             bool checkvote(){
                 if(voterewo>10000){return true;}
                 else{return false;}
             }
-            bool checkaudit(){
-                if(auditrewo>1){return true;}
-                else{return false;}
-            }
-            EOSLIB_SERIALIZE(catepot,(catename)(totalvote)(totalrewo)(bestrewo)(voterewo)(auditrewo)(buyramrewo))
+            EOSLIB_SERIALIZE(catetick,(catename)(paytick)(votetick)(addtick)(voterewo)(auditrewo))
         };
-        typedef multi_index<N(catepots),catepot,
-            indexed_by<N(byvote),const_mem_fun<catepot,uint64_t,&catepot::get_vote>>,
-            indexed_by<N(byrewo),const_mem_fun<catepot,uint64_t,&catepot::get_rewo>>
-            > catepots;
+        typedef multi_index<N(cateticks),catetick,
+            indexed_by<N(byvote),const_mem_fun<catetick,uint64_t,&catetick::get_vote>>,
+            indexed_by<N(bypay) ,const_mem_fun<catetick,uint64_t,&catetick::get_pay>>
+            > cateticks;
 
         // scope为accountname
         // @abi table unstakes i64
@@ -101,6 +140,22 @@ class wafyartvotes : public eosio::contract {
         };
         typedef multi_index<N(senderids),senderid> senderids;
 
+        // scope为_self alltick 账户余额 = 抵押票（已投+未投+支付）+ 增发票
+        // @abi table allticks i64
+        struct alltick{
+            account_name byname;
+            uint64_t paytick;                       // 该项目总共获得的收益，来自于各个分类，支付的赏金
+            uint64_t votetick;                      // 该项目抵押的票中，投出去的数目
+            uint64_t unvotetick;                    // 该项目抵押的票中，未投出去的数目
+            uint64_t unstaketick;                   // 该项目中解锁中的票
+            uint64_t addtick;                       // 该项目增发的票
+
+            uint64_t buyramtick;                    // 用作购买ram的票 基于该项目，不基于分类，更不基于项目
+
+            uint64_t primary_key()const {return byname;}
+        }
+        typedef multi_index<N(allticks),alltick> allticks;
+
         // @abi action
         void staketit(account_name byname,account_name accname,uint64_t amount);
         // @abi action
@@ -109,6 +164,11 @@ class wafyartvotes : public eosio::contract {
         void setstats(account_name byname,account_name accname,uint64_t id);
         // @abi action
         void delunstake(account_name byname,uint64_t id);
+        // @abi action
+        void voteart(account_name byname,uint64_t votenum,account_name catename,uint64_t artid);
+        // @abi action
+        //void createart(account_name byname,)
+        
     private:
         uint128_t getid(uint64_t id);
         
